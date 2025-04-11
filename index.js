e3b0ac6526fd8349a5e4bd8338b507d73ab4b75e
Chris Najman <christopher.najman@gmail.com>
2025-04-11 09:45:45 +0100

    Added responsive menu with hamburger button

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import themeSwitcher from "./js-modules/theme.js"
 import loadingAnimation from "./js-modules/loader.js"
+import btnHamburger from "./js-modules/primary-navigation/hamburger-button.js"
+import primaryNavLinks from "./js-modules/primary-navigation/primary-nav-links.js"
 import ariaExpandedDetails from "./js-modules/aria-expanded-details.js"
 
 themeSwitcher()
 loadingAnimation()
+btnHamburger()
+primaryNavLinks()
 ariaExpandedDetails()
