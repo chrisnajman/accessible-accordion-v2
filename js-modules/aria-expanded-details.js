22bdc1899264c2b6c64080b4d69dc30018ca6bc3
Chris Najman <christopher.najman@gmail.com>
2025-09-10 13:41:48 +0100

    aria-expanded-details.js - 1) Global event listener. 2) aria-expanded moved to summary. 3) aria-expanded initialised. 4) Escape key event listener added.

--- js-modules/aria-expanded-details.js
+++ js-modules/aria-expanded-details.js
@@ -1,7 +1,26 @@
 export default function ariaExpandedDetails() {
+  // Applies to all <details> elements
   document.querySelectorAll("details").forEach((detail) => {
+    const summary = detail.querySelector("summary")
+
+    // Initialize aria-expanded based on initial state
+    summary.setAttribute("aria-expanded", detail.open ? "true" : "false")
+
+    // Set aria-expanded attribute for all details elements dynamically
     detail.addEventListener("toggle", () => {
-      detail.setAttribute("aria-expanded", detail.open ? "true" : "false")
+      summary.setAttribute("aria-expanded", detail.open ? "true" : "false")
+    })
+
+    // Close the <details> element when Escape key is pressed while focus is inside it.
+    // Accessibility: Returns focus to the <summary> after closing.
+    detail.addEventListener("keydown", (e) => {
+      if (e.key === "Escape") {
+        if (detail.open) {
+          detail.removeAttribute("open")
+          summary.setAttribute("aria-expanded", "false")
+          summary.focus() // return focus to summary
+        }
+      }
     })
   })
 }
